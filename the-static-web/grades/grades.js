d9825452b78d10cc652520a8e0ad73149beec80a
Theo Carnavos <tcarnavos@gmail.com>
2016-01-10 16:49:37 -0600

    completed JS challenge

---
+++ the-static-web/grades/grades.js
@@ -0,0 +1,49 @@
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var gradeA = 0;
+var gradeB = 0;
+var gradeC = 0;
+var gradeD = 0;
+var gradeF = 0;
+
+function gradeAssign (array) {
+	for (i = 0; i < array.length; i++) {
+		// define currently viewed array element
+		var currentScore = array[i];
+		// if/else to determine grade count
+		if (currentScore > 90) {
+			gradeA++;
+		} else if (currentScore > 80) {
+			gradeB++;
+		} else if (currentScore > 70) {
+			gradeC++;
+		} else if (currentScore > 60) {
+			gradeD++;
+		} else {
+			gradeF++;
+		}
+	}
+	
+	// Print amount of each grade
+	console.log("Amount of A's: " + gradeA);
+	console.log("Amount of B's: " + gradeB);
+	console.log("Amount of C's: " + gradeC);
+	console.log("Amount of D's: " + gradeD);
+	console.log("Amount of F's: " + gradeF);
+}
+
+gradeAssign(scores);
+
+// experimental prototype addition of the math min and max functions
+Array.prototype.max = function() {
+	return Math.max.apply(null, this);
+}
+Array.prototype.min = function() {
+	return Math.min.apply(null, this);
+}
+
+// Print lowest and highest scores
+
+console.log("Lowest grade: " + scores.min());
+console.log("Highest grade: " + scores.max());
