7fd6b56f8a60527683d8e5384bc87df9978879e5
Theo Carnavos <tcarnavos@gmail.com>
2016-04-21 15:10:08 -0500

    initial Alphabits setup

---
+++ csharp/Alphabits/Alphabits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter each letter of the alphabet lowercase in order from a to z");
+        }
+    }
+}
+//Alphabits
+//Use visual studio and C# to build a console app where the user is challenged to enter all the letters of the alphabet, one at a time, without repeating any, and without typing any non-letter characters.
+
+//Behavior
+
+//After each successful input, display the number of letters already entered.
+//After each unsuccessful input, display a helpful message explaining why the input was unsuccessful(e.g. 'not a letter', 'duplicate letter', etc.)
+//Upon completing the task(entering in all 26 letters), the user should receive some gratifying message like "Congratulations"
+//Technical requirements
+
+//Use one Class in addition to the program file.
+//Your class should include a constructor function, an "addChar" method, and a "listLength" method.
+//Keep your user's successfully input letters as a list of characters
+//Use the String.Format method to craft your messages to users
+//Bonus Requirements
+
+//Create a returnList Method on your class
+//Create a non-letter "Easter egg" character that will display the current list of successfully input letters(but will not add itself to the list!)
---
+++ csharp/Alphabits/Alphabits/alphabetClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabits
+{
+    // Your class should include a constructor function, an "addChar" method, and a "listLength" method.
+    // Keep your user's successfully input letters as a list of characters
+    // Use the String.Format method to craft your messages to users
+    class alphabetClass
+    {
+        public void addChar (char character, string alphabet)
+        {
+            // logic to add character to the larger string
+        }
+
+        public void listLength (List alphaList)
+        {
+            // logic to display the length of the alphabet List as is currently
+        }
+    }
+}
